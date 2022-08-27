a4c250b8e2525e3962f61108b810403fae5a51e4
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-08-27 22:58:42 +0300

    formatted pseudo code

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,7 +1,7 @@
 
 public class ParenthesesChecker {
 
-    // pseudo code
+//    pseudo code
 
 // create a new stack for storing relevant testString input
 
@@ -10,16 +10,16 @@ public class ParenthesesChecker {
     // break up testString into an array of individual characters
     // check if array length is uneven number (i.e., number of brackets is uneven) - return false
     // initiate a for loop to assess each element in new character array
-    // retrieve an individual character from array
-    // if character '('
-    // push to stack if stack is empty or top element is '('
-    // if character ')' & stack empty - return false
-    // if character ')' & top stack element is '('
-    // push to stack
-    // if stack size even number - return true
-    // if character ')' & top stack element is ')'
-    // push to stack
-    // if stack size even number - return true
+        // retrieve an individual character from array
+        // if character '('
+            // push to stack if stack is empty or top element is '('
+        // if character ')' & stack empty - return false
+        // if character ')' & top stack element is '('
+            // push to stack
+            // if stack size even number - return true
+        // if character ')' & top stack element is ')'
+            // push to stack
+            // if stack size even number - return true
     // check if stack is empty
-    
+
 }
