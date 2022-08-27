bbd736583b48c79afc4c4313a07764e262aa4c7f
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-08-27 16:34:21 +0300

    writing initial pseudo code

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,5 +1,25 @@
 public class ParenthesesChecker {
 
+    private String checker;
 
+    // pseudo code
+
+    // create a reference ArrayList with opening and closing brackets
+        // one pair reference ()
+        // nested reference (())
+
+    // create a checkParentheses method
+        // create an empty ArrayList
+        // populate the ArrayList by adding one element at a time
+            // if empty ArrayList
+                // return true
+            // check the number of brackets - length of input
+                // if uneven number
+                    // return false
+                // if even number
+                    // check if matches to one pair reference or nested reference
+                        // return true
+                    // else
+                        // return false
 
 }
