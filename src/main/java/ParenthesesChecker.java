bdcf2ee5934ddcad47d658460fd7a48d87659d4b
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-08-27 22:55:11 +0300

    updated pseudo code

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,25 +1,25 @@
-public class ParenthesesChecker {
 
-    private String checker;
+public class ParenthesesChecker {
 
     // pseudo code
 
-    // create a reference ArrayList with opening and closing brackets
-        // one pair reference ()
-        // nested reference (())
+// create a new stack for storing relevant testString input
 
-    // create a checkParentheses method
-        // create an empty ArrayList
-        // populate the ArrayList by adding one element at a time
-            // if empty ArrayList
-                // return true
-            // check the number of brackets - length of input
-                // if uneven number
-                    // return false
-                // if even number
-                    // check if matches to one pair reference or nested reference
-                        // return true
-                    // else
-                        // return false
-
+// create a checkParentheses method
+    // if testString empty - return true
+    // break up testString into an array of individual characters
+    // check if array length is uneven number (i.e., number of brackets is uneven) - return false
+    // initiate a for loop to assess each element in new character array
+    // retrieve an individual character from array
+    // if character '('
+    // push to stack if stack is empty or top element is '('
+    // if character ')' & stack empty - return false
+    // if character ')' & top stack element is '('
+    // push to stack
+    // if stack size even number - return true
+    // if character ')' & top stack element is ')'
+    // push to stack
+    // if stack size even number - return true
+    // check if stack is empty
+    
 }
